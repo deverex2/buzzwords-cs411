9929826c868e46dc380df1a64a57e52c9b0de29a
Devereux <deverex2@sp19-cs411-27.cs.illinois.edu>
2019-03-30 16:19:40 -0500

    basic endpoints progress

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+from flask import Response
 import MySQLdb
 
 app = Flask(__name__)
@@ -6,9 +8,51 @@ app = Flask(__name__)
 def main():
     db = MySQLdb.connect(host="localhost",user="root", passwd="",db="Project")
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM members""")
+    cursor.execute("""SHOW TABLES""")
     data = cursor.fetchall()
     return str(data)
+
+@app.route('/artist')
+def artist():
+    name = request.args.get('name')
+    db = MySQLdb.connect(host="localhost",user="root", passwd="",db="Project")
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM Artists WHERE name=%s""", (name,))
+    data = cursor.fetchall()
+    resp = Response(str(data))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+    
+@app.route('/song')
+def song():
+    title = request.args.get('title')
+    db = MySQLdb.connect(host="localhost",user="root", passwd="",db="Project")
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM Songs WHERE title=%s""", (title,))
+    data = cursor.fetchall()
+    resp = Response(str(data))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+@app.route('/search')
+def search():
+    gram = request.args.get('gram')
+    groupby = request.args.get('groupby')
+    db = MySQLdb.connect(host="localhost",user="root", passwd="",db="Project")
+    cursor = db.cursor()
+    if groupby=='year':
+        cursor.execute("""SELECT year, SUM(l_freq) FROM ((SELECT * FROM Lyrics WHERE words = %s) S1 NATURAL JOIN (SELECT s_id, year FROM Songs) S2) GROUP BY year;""", (gram,))
+    elif groupby=='artist':
+        cursor.execute("""SELECT * FROM Artists""")
+    elif groupby=='genre':
+        cursor.execute("""SELECT * FROM Genres""")
+    else:
+        return "-1"
+    data = cursor.fetchall()
+    resp = Response(str(data))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+ 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
